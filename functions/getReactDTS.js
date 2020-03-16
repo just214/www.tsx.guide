0bf4ec8caff486f627c51c750b193b3fe5080230
gojutin <gojutin@gmail.com>
2020-03-16 19:19:17 -0400

    Updated Netlify function

--- functions/getReactDTS.js
+++ functions/getReactDTS.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 exports.handler = async (event, context) => {
   const type = event.queryStringParameters.type;
-  const res = parse(require.resolve("./Test.tsx"));
+  const res = parse("./Test.tsx");
 
   const result = res.find(({ displayName }) => displayName === type).props;
 
