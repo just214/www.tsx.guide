cb948da4f66c45b0d540e52c798ae5cc4700714e
gojutin <gojutin@gmail.com>
2020-03-16 20:07:32 -0400

    Bug fix

--- functions/getReactDTS.js
+++ functions/getReactDTS.js
@@ -1,13 +1,12 @@
 require("typescript");
 const { parse } = require("react-docgen-typescript").withDefaultConfig();
-const Module = module.constructor;
 const path = require("path");
 
-const lll = path.resolve(__dirname + '/Test.tsx')
+// const lll = require.resolve('./Test.tsx')
 
 exports.handler = async (event, context) => {
   const type = event.queryStringParameters.type;
-  const res = parse(lll);
+  const res = parse("./Test.tsx");
   const result = res.find(({ displayName }) => displayName === type).props;
 
   return {
